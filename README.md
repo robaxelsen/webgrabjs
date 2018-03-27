63a03d6c5a5d24b2e800bf2e771327df3a044e1a
Robert Axelsen <rob@rob.ee>
2018-03-27 20:52:24 +0200

    Implement initial poc

---
+++ README.md
@@ -0,0 +1,22 @@
+# Webgrab.js
+
+Really simple CLI tool to quickly grab a screenshot of the front page of a website.
+
+### Installation
+
+Node v8 or greater is required.
+
+1. Clone the repo
+`git clone https://github.com/robaxelsen/webgrabjs.git`
+1. Enter the repo directory
+`cd webgrabjs`
+1. Install npm package dependencies
+`npm install`
+
+### Usage
+
+Syntax:
+`node webgrab.js <site> [dimensions]`
+
+Example:
+`node webgrab.js https://github.com/robaxelsen/webgrabjs 1920x1080`
---
+++ webgrab.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer');
+const domain = require('getdomain');
+const args = process.argv;
+
+const dimensions = args[3].split('x');
+const height = parseInt(dimensions[0]) ? parseInt(dimensions[0]) : 1920;
+const width = parseInt(dimensions[1]) ? parseInt(dimensions[1]) : 1080;
+
+const url = args[2];
+const currentDate = new Date().toISOString().replace(/T/, '_').replace(/\..+/, '').replace(/:/g, '-');
+const filepath = `screenshots/${domain.get(url)}_${currentDate}.png`;
+
+(async () => {
+  let browser = await puppeteer.launch();
+  let page = await browser.newPage();
+  page.setViewport({ width: width, height: height });
+
+  await page.goto(url);
+  await page.screenshot({path: filepath});
+
+  await browser.close();
+})();
